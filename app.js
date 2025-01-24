877a4e14070abfc8f1b2a69c635691f3e9a23a60
Danyl-Dudka <danyad.stokes@gmail.com>
2025-01-24 02:11:03 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+const interval = 3000;
+let currentSlide = 0;
+const slidesElement = document.querySelector('.slides');
+let slideInterval = setInterval(nextSlide, interval);
+const nextButton = document.querySelector('.next-btn');
+const prevButton = document.querySelector('.prev-btn');
+const indicator = document.querySelector('.indicators').children;
+let start;
+let end;
+function resetSlideInterval() {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, interval);
+}
+function nextSlide() {
+    clearInterval(slideInterval);
+    slidesElement.children[currentSlide].classList.remove('active');
+    indicator[currentSlide].classList.remove('active');
+    if (currentSlide === slidesElement.children.length - 1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+    slidesElement.children[currentSlide].classList.add('active')
+    indicator[currentSlide].classList.add('active');
+    slideInterval = setInterval(nextSlide, interval);
+}
+function stopShowing() {
+    let isPlaying = true;
+    document.addEventListener('keydown', (event) => {
+        if (event.code === "Space") {
+            if (isPlaying) {
+                clearInterval(slideInterval);
+                console.log("Slider is stopped");
+            } else {
+                slideInterval = setInterval(nextSlide, interval);
+                console.log("Slider is playing");
+            }
+            isPlaying = !isPlaying;
+        }
+    })
+}
+
+function prevSlide() {
+    clearInterval(slideInterval);
+    slidesElement.children[currentSlide].classList.remove('active');
+    indicator[currentSlide].classList.remove('active');
+    if (currentSlide === 0) {
+        currentSlide = slidesElement.children.length - 1;
+    } else {
+        currentSlide--;
+    }
+    slidesElement.children[currentSlide].classList.add('active');
+    indicator[currentSlide].classList.add('active');
+    resetSlideInterval();
+}
+
+function changeSlides() {
+    nextButton.addEventListener('click', nextSlide);
+    prevButton.addEventListener('click', prevSlide);
+}
+
+function handleKeyboardControl() {
+    document.addEventListener('keydown', (event) => {
+        if (event.code === 'ArrowRight') {
+            nextSlide();
+        } if (event.code === 'ArrowLeft') {
+            prevSlide();
+        }
+    })
+}
+
+function activateSlide(index) {
+    clearInterval(slideInterval);
+    slidesElement.children[currentSlide].classList.remove('active');
+    indicator[currentSlide].classList.remove('active');
+
+    currentSlide = index;
+
+    slidesElement.children[currentSlide].classList.add('active');
+    indicator[currentSlide].classList.add('active');
+    resetSlideInterval();
+}
+
+function activateIndicator() {
+    for (let i = 0; i < indicator.length; i++) {
+        indicator[i].addEventListener('click', () => {
+            activateSlide(i);
+        })
+    }
+}
+slidesElement.addEventListener('touchstart', (event) => {
+    clearInterval(slideInterval);
+    start = event.touches[0].clientX;
+    event.preventDefault();
+})
+slidesElement.addEventListener('touchend', (event) => {
+    end = event.changedTouches[0].clientX;
+    const diff = end - start;
+    if (diff > 50) {
+        prevSlide();
+    } else if (diff < -50) {
+        nextSlide();
+    }
+    event.preventDefault();
+    resetSlideInterval();
+})
+
+activateIndicator();
+handleKeyboardControl();
+changeSlides();
+stopShowing();
